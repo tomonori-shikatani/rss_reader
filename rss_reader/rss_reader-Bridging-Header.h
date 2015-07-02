bc440e748c2ecfe006152449cf3358522032125e
ShikataniTomonori <shikatani.0902@gmail.com>
2015-07-02 15:46:06 +0900

    Bridging-Header.hを作成

---
+++ rss_reader/rss_reader-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  rss_reader-Bridging-Header.h
+//  rss_reader
+//
+//  Created by ShikataniTomonori on 2015/07/02.
+//  Copyright (c) 2015年 ShikataniTomonori. All rights reserved.
+//
+
+#ifndef rss_reader_rss_reader_Bridging_Header_h
+#define rss_reader_rss_reader_Bridging_Header_h
+
+#import <SVProgressHUD.h>
+#import <MWFeedParser.h>
+
+
+#endif
